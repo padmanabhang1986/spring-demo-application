2806cf79a694aeb444d19304b07be329456aeb0e
Padmanabhan Gopalan <padmanabhang1986@gmail.com>
2021-02-26 01:31:47 +0530

    Signed-off-by: Padmanabhan Gopalan <padmanabhang1986@gmail.com>

--- src/main/java/com/padmanabhan/samplespringproject/controllers/SampleController.java
+++ src/main/java/com/padmanabhan/samplespringproject/controllers/SampleController.java
@@ -1,2 +1,12 @@
-package com.padmanabhan.samplespringproject.controllers;public class SampleController {
+package com.padmanabhan.samplespringproject.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+    @GetMapping
+    public String sendMessage(){
+        return "Hello World Spring Boot Program";
+    }
 }
